3fdbd3fb678e90e5ead3ca60c76a4fa8ba16ea14
Alina-Jianu-JCMaxwell-5Bi <alina.jianu@jcmaxwell.it>
2017-01-22 16:11:52 +0100

    Aggiunta commenti mancanti
    Dani, questo è fatto.

--- Client/ClientTesto.java
+++ Client/ClientTesto.java
@@ -6,13 +6,15 @@
 
 /**
  *
- * @author Prof. Matteo Palitto
+ * @author Professore Matteo Palitto
  * @author Jianu Alina
  * @author Ragusa Daniel
  * @author Stropiana Alessandro
  */
-import java.net.*;
-import java.io.*;
+
+//librerie
+import java.net.*; //sarebbe "java.net.Socket", ma siccome sono state incluse tutte le classi, si mette '*'
+import java.io.*; //serve per la gestione dei files
 
 public class ClientTesto {
     
@@ -23,59 +25,83 @@ public class ClientTesto {
     
     public static void main(String[] args) {
 	// verifica correttezza dei parametri
-	if (args.length != 2) {
+	if (args.length != 2) 
+	{
+	    //messaggio di output
             System.out.println("Uso: java client-Testo <indirizzo IP Server> <Porta Server>");
             return;
         }
-	
-	    String nickname = null;
+	// inserimento del nickname
+	String nick = null;
         try{
+	    //messaggio di output
             System.out.println("Inserisci il nickname");
-            nickname = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-        } catch(IOException e) 
-            { System.out.println("I/O Error");
-                                 System.exit(-1); }
-	  
+	    //lettura del file tramite un buffer
+            nick = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+        } 
+	//eccezione  
+	catch(IOException e) 
+	{ 
+		//messaggio di output
+		System.out.println("I/O Error");
+                System.exit(-1); 
+	}	
+	
+	//dichiarazione della stringa "hostName"
 	String hostName = args[0];
+	//parsificazione
 	int portNumber = Integer.parseInt(args[1]);
+	    
 	try {
-            // prendi l'indirizzo IP del server dalla linea di comando
+            //viene preso l'indirizzo IP del server dalla linea di comando
             InetAddress address = InetAddress.getByName(hostName);
 			
-            // creazione socket 
+            // creazione di uno socket 
             Socket clientSocket = new Socket(address, portNumber);
 		
-            // visualizza istruzioni
-            System.out.println("Client-Testo: usa Ctrl-C per terminare, ENTER per spedire la linea di testo.\n");
+            //visualizzazione delle istruzioni
+            System.out.println("Client-Testo: usa EXIT per terminare, ENTER per spedire la linea di testo.\n");
 			
-            // connessione concorrente al socket per ricevere i dati da Server
+            //connessione concorrente al socket per ricevere i dati da Server
             listener l;
             try {
                 l = new listener(clientSocket);
                 Thread t = new Thread(l);
                 t.start();
-            } catch (Exception e) { System.out.println("Connessione NON riuscita con server: "); }
+            } 
+		
+	    catch (Exception e) 
+	    { 
+		    System.out.println("Connessione NON riuscita con server: "); 
+	    }
 		
             // connessione al socket (in uscita client --> server)
             PrintWriter out =  new PrintWriter(clientSocket.getOutputStream(), true);
 			
             // connessione allo StdIn per inserire il testo dalla linea di comando
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-            String userInput;
             
-            //leggi da linea di comando il testo da spedire al Server
+	    String userInput;
+            // il primo stream è rappresentato dal nickname
+            out.println(nick);
+		
+            //lettura da linea di comando del testo da spedire al Server
             System.out.print(">"); //visualizza il prompt
-            while ((userInput = stdIn.readLine()) != null) {
+            while ((userInput = stdIn.readLine()) != null) 
+	    {
             	// scrittura del messaggio da spedire nel socket 
 		out.println(userInput);
                 System.out.println("Messaggio spedito al server: " + userInput);
                 System.out.print(">"); //visualizza il prompt
             }
+		
             // chiusura socket
             clientSocket.close();
             System.out.println("connessione terminata!");
 	}
-        catch (IOException e) { System.out.println("Connessione terminata dal server: "); e.printStackTrace(); }
-    }
+        catch (IOException e) 
+	{ 
+		System.out.println("Connessione terminata dal server: "); e.printStackTrace(); }
+    	}
     
 }
